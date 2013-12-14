65145a5f5d97cb2f14b954bb13f5e7cdbb5ae66a
veget88 <pierpaolo.rametta@gmail.com>
2013-12-14 12:10:25 +0100

    Better choose of direction

--- VacuumES_EnvironmentB/src/explorer/ExplorerMushroomHunter.java
+++ VacuumES_EnvironmentB/src/explorer/ExplorerMushroomHunter.java
@@ -26,8 +26,7 @@ public class ExplorerMushroomHunter implements ExplorerInterface {
 	private List<Point> currentFront;
 	private int currentFrontDistance;
 	private List<Point> unreacheables;
-
-
+	private boolean clockwise;
 
 	public ExplorerMushroomHunter (AgentProgramES agent) {
 		this.agent = agent;
@@ -35,6 +34,7 @@ public class ExplorerMushroomHunter implements ExplorerInterface {
 		path = null;
 		currentFront = new LinkedList<Point>();
 		unreacheables = new LinkedList<Point>();
+		this.clockwise = true;
 	}
 
 	@Override
@@ -47,52 +47,91 @@ public class ExplorerMushroomHunter implements ExplorerInterface {
 
 	@Override
 	public Movement nextAction() {
-		
+
 		if (map.isCompletelyExplored())
 			return null;
-		
+
 		Point current = map.getCurrentPositionPoint();
 		List<Point> adj = new LinkedList<Point>();
 		List<Point> unexploredPoints = map.getUnexploredPoints();
-		
+
 		if (path != null && path.size() > 0) 
 			return MapUtils.movementFromTwoPoints(current, path.remove(0));
 
-		
+
 		updateFront();
-		
+
 		checkReachability();
-		
+
 		if (path != null && path.size() > 0)
 			return MapUtils.movementFromTwoPoints(current, path.remove(0));
-		
+
 		/* Expand front */
 		if (currentFront.size() == 0) 
 			expandFront();
 		
-		
-		for (Point p : map.getAdjWalkablePoints(map.getCurrentPositionPoint())) 
-			if (currentFront.contains(p))
-				return chooseRouteNextMovement(p);
-		
-		
+		/* Control of direction */
+		Movement m = directionControl();
+		if (m != null)
+			return m;
 		
 		Point p;
 		p = findNearestPoint();
 		while(p == null){
-			
 			unreacheables.addAll(currentFront);
 			currentFront.clear();
 			expandFront();
 			p = findNearestPoint();
 		} 
-		
-			
-		
-		
-		Movement m = this.chooseRouteNextMovement(path.remove(0)); //TODO change name
-						
+		m = this.chooseRouteNextMovement(path.remove(0)); //TODO change name
+
 		return m;
+	}
+	
+	public Movement directionControl () {
+		Point current = map.getCurrentPositionPoint();
+		
+		List<Point> walkable = map.getAdjWalkablePoints(map.getCurrentPositionPoint());
+		for (Point p : map.getAdjWalkablePoints(current)) 
+			if (!currentFront.contains(p))
+				walkable.remove(p);
+
+		/* Direction control */
+		if (walkable.size() == 2){
+			if (clockwise) {
+				if (MapUtils.movementFromTwoPoints(map.getCurrentPositionPoint(), walkable.get(0)) == Movement.right ||
+						MapUtils.movementFromTwoPoints(map.getCurrentPositionPoint(), walkable.get(0)) == Movement.up) {
+					return MapUtils.movementFromTwoPoints(current, walkable.get(0));
+				} else {
+					return MapUtils.movementFromTwoPoints(current, walkable.get(1));
+				}
+			}else 
+				if (MapUtils.movementFromTwoPoints(map.getCurrentPositionPoint(), walkable.get(0)) == Movement.left ||
+				MapUtils.movementFromTwoPoints(map.getCurrentPositionPoint(), walkable.get(0)) == Movement.down) {
+					return MapUtils.movementFromTwoPoints(current, walkable.get(0));
+				} else {
+					return MapUtils.movementFromTwoPoints(current, walkable.get(1));
+				}
+		} else if (walkable.size() == 1) {
+			if (clockwise)
+				if (MapUtils.movementFromTwoPoints(current, walkable.get(0)) == Movement.up ||
+				MapUtils.movementFromTwoPoints(current, walkable.get(0)) == Movement.right)
+					return MapUtils.movementFromTwoPoints(current, walkable.get(0));
+				else {
+					clockwise = false;
+					return MapUtils.movementFromTwoPoints(current, walkable.get(0));
+				}
+			else
+				if (MapUtils.movementFromTwoPoints(current, walkable.get(0)) == Movement.down ||
+				MapUtils.movementFromTwoPoints(current, walkable.get(0)) == Movement.left)
+					return MapUtils.movementFromTwoPoints(current, walkable.get(0));
+				else {
+					clockwise = true;
+					return MapUtils.movementFromTwoPoints(current, walkable.get(0));
+				}
+		}
+		
+		return null;
 	}
 
 	private void checkReachability() {
@@ -100,14 +139,14 @@ public class ExplorerMushroomHunter implements ExplorerInterface {
 		urc.addAll(unreacheables);
 		List<Point> unexplored = map.getUnexploredPoints();
 		Astar astar = new Astar(map);
-		
+
 		for (Point point : urc) {
 			/* Maybe these is not necessary TODO */
 			if (map.isVisited(point)) {
 				unreacheables.remove(point);
 				continue;
 			}
-			
+
 			if (unexplored.contains(point)) {
 				/* Should be always ad adjacent point to out current position*/
 				unreacheables.remove(point);
@@ -131,14 +170,14 @@ public class ExplorerMushroomHunter implements ExplorerInterface {
 		cfc.addAll(currentFront);
 		for (Point p : cfc) {
 			if (!map.getUnexploredPoints().contains(p))
-					continue;
-			
+				continue;
+
 			astar.astar(map.getCurrentPositionPoint(), p);
 			pathToN = astar.getPointPath();
-			
+
 			if (pathToN.size() == 0)
 				continue;
-			
+
 			if (pathToN.size() < min) { 
 				ret = p;
 				min = pathToN.size();
@@ -146,7 +185,7 @@ public class ExplorerMushroomHunter implements ExplorerInterface {
 				retPath.addAll(pathToN);
 			}
 		}
-		
+
 		path = retPath;
 		return ret;
 	}
@@ -155,7 +194,7 @@ public class ExplorerMushroomHunter implements ExplorerInterface {
 	private void updateFront() {
 		LinkedList<Point> cfc = new LinkedList<Point>();
 		cfc.addAll(currentFront);
-		
+
 		for (Point p : cfc) 
 			if(map.isVisited(p))
 				currentFront.remove(p);
@@ -191,10 +230,10 @@ public class ExplorerMushroomHunter implements ExplorerInterface {
 		}
 
 	}
-	
+
 	private void expandFront() {
 		int d = ++currentFrontDistance;
-		
+
 		Point p;
 		for (int i = start.x - d; i <= start.x + d; i++) {
 			p = new Point(i, start.y+d);
@@ -204,7 +243,7 @@ public class ExplorerMushroomHunter implements ExplorerInterface {
 			if (!map.isVisited(p) && !currentFront.contains(p))
 				currentFront.add(p);
 		}
-		
+
 		for (int i = start.y - d; i <= start.y + d; i++) {
 			p = new Point(start.x+d, i);
 			if (!map.isVisited(p) && !currentFront.contains(p))
@@ -213,12 +252,12 @@ public class ExplorerMushroomHunter implements ExplorerInterface {
 			if (!map.isVisited(p) && !currentFront.contains(p))
 				currentFront.add(p);
 		}
-		
+
 	}
 	private void findPathToPoint(Point dest) {
 		Astar a = new Astar(map);
 		path = a.astar(map.getCurrentPositionPoint(), dest).getPointPath();
 	}
-	
+
 
 }
